16c74f299a5db2bc88df1adf46207415f7060eb5
VuChiCong <92044924+VuChiDoanh@users.noreply.github.com>
2023-09-26 23:58:55 +0700

    Create router of tour

--- BE-VCDTT/routes/api.php
+++ BE-VCDTT/routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::prefix('admin')->group(function () {
+    // Tour management
+    Route::get('/tour', [ApiStudentController::class, 'index']);
+    Route::post('/tour-store', [ApiStudentController::class, 'store']);
+    Route::get('/tour-show/{id}', [ApiStudentController::class, 'show']);
+    Route::put('/tour-edit/{id}', [ApiStudentController::class, 'update']);
+    Route::delete('/tour-destroy/{id}', [ApiStudentController::class, 'destroy']);
+    
+});
