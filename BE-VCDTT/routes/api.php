ab9fcf852c64120a63f1a954015b808a0bfb7ac4
chinhph28756 <chinhddph28756@fpt.edu.vn>
2023-09-27 03:24:31 +0700

    Create view edit func and edit func

--- BE-VCDTT/routes/api.php
+++ BE-VCDTT/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\TourController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('admin')->group(function () {
+    // Tour management
+    Route::get('/tour', [TourController::class, 'index']);
+    Route::post('/tour-store', [TourController::class, 'store']);
+    Route::get('/tour-show/{id}', [TourController::class, 'show']);
+    Route::put('/tour-edit/{id}', [TourController::class, 'update']);
+    Route::delete('/tour-destroy/{id}', [TourController::class, 'destroy']);
 });
