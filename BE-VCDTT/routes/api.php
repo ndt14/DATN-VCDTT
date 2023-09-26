b01ffb07f9c11f68012400c6317b33cc92daf883
VuChiCong <92044924+VuChiDoanh@users.noreply.github.com>
2023-09-27 00:03:20 +0700

    fix controller location

--- BE-VCDTT/routes/api.php
+++ BE-VCDTT/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\TourController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,10 +20,10 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 });
 Route::prefix('admin')->group(function () {
     // Tour management
-    Route::get('/tour', [ApiStudentController::class, 'index']);
-    Route::post('/tour-store', [ApiStudentController::class, 'store']);
-    Route::get('/tour-show/{id}', [ApiStudentController::class, 'show']);
-    Route::put('/tour-edit/{id}', [ApiStudentController::class, 'update']);
-    Route::delete('/tour-destroy/{id}', [ApiStudentController::class, 'destroy']);
-    
+    Route::get('/tour', [TourController::class, 'index']);
+    Route::post('/tour-store', [TourController::class, 'store']);
+    Route::get('/tour-show/{id}', [TourController::class, 'show']);
+    Route::put('/tour-edit/{id}', [TourController::class, 'update']);
+    Route::delete('/tour-destroy/{id}', [TourController::class, 'destroy']);
+
 });
