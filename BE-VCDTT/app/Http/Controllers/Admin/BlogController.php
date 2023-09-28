29db49072b85d43ed4cdc32339576f267f6919e9
TTSCool <vietdainhan1@gmail.com>
2023-09-28 21:14:54 +0700

    search tour

--- BE-VCDTT/app/Http/Controllers/Admin/BlogController.php
+++ BE-VCDTT/app/Http/Controllers/Admin/BlogController.php
@@ -3,16 +3,27 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BlogController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $keyword = trim($request()->keyword);
+        $sql_where ='';
+        // $sql_where=' AND delete_at IS NULL';
+        if(!empty($keyword)){
+            $sql_where .= ' AND tour_name LIKE %{$keyword}%';
+        }
+        $sql_order =' ORDER BY DESC';
+        $sql_limit =' LIMIT 9';
+        $blog = DB::select('select * from blos where 1'.$sql_where.$sql_order.$sql_limit);
+        return response()->json();
     }
 
     /**
@@ -20,7 +31,7 @@ class BlogController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $blog = Blog::create($request->all());
     }
 
     /**
@@ -28,7 +39,7 @@ class BlogController extends Controller
      */
     public function show(string $id)
     {
-        //
+        
     }
 
     /**
