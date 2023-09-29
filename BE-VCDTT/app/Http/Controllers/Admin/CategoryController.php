7a55482796053f39154e4738bd8507cd6c9240e7
VuChiCong <92044924+VuChiDoanh@users.noreply.github.com>
2023-09-29 22:32:49 +0700

    list & search

--- BE-VCDTT/app/Http/Controllers/Admin/CategoryController.php
+++ BE-VCDTT/app/Http/Controllers/Admin/CategoryController.php
@@ -5,13 +5,26 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
 use App\Http\Resources\CategoryResource;
 
 class CategoryController extends Controller
 {
-   public function index()
+    public function index(Request $request)
     {
-        //
+        $categories = new Category;
+
+        $keyword = trim($request->keyword) ? trim($request->keyword) : '';
+        $limit = intval($request->limit);
+        $sql_order = 'name';
+
+        $categoriesParent = $categories->getCategoriesParent($keyword, $sql_order, $limit);
+
+        foreach($categoriesParent as $parent){
+            $parent->Child = $categories->getCategoriesChild($parent->id);
+        }
+
+        return response()->json(['dataCategoriesParent' => CategoryResource::collection($categoriesParent)], 200);
 
     }
 
