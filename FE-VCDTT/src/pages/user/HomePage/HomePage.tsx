ab386366e01ab6379e0dd14c03f78d424eea9ff2
thaivhph28695 <hongthai96961996@gmail.com>
2023-10-02 20:07:51 +0700

    add modal signin and signup

--- FE-VCDTT/src/pages/user/HomePage/HomePage.tsx
+++ FE-VCDTT/src/pages/user/HomePage/HomePage.tsx
@@ -13,22 +13,9 @@ const HomePage = () => {
           <div className="home-banner-items">
             <div className="banner-inner-wrap">
               <Carousel autoplay>
-                <img
-                  src="https://theme.hstatic.net/1000376021/1000834008/14/slideshow_4.jpg?v=3691"
-                  alt=""
-                />
-                <img
-                  src="https://theme.hstatic.net/1000376021/1000834008/14/slideshow_5.jpg?v=3691"
-                  alt=""
-                />
-                <img
-                  src="https://theme.hstatic.net/1000376021/1000834008/14/slideshow_6.jpg?v=3691"
-                  alt=""
-                />
-                <img
-                  src="https://file.hstatic.net/1000376021/file/1920x720_copy_42b3f822c4ca4cd099bfb116931e6361.png"
-                  alt=""
-                />
+                <img src="assets/images/slider-banner-1.jpg" alt="" />
+
+                <img src="assets/images/slider-banner-2.jpg" alt="" />
               </Carousel>
             </div>
             <div className="banner-content-wrap">
@@ -63,130 +50,7 @@ const HomePage = () => {
         </div>
       </div>
       {/* <!-- search search field html end --> */}
-      {/* <section className="destination-section">
-        <div className="container">
-          <div className="section-heading">
-            <div className="row align-items-end">
-              <div className="col-lg-7">
-                <h2>TOP NOTCH DESTINATION</h2>
-              </div>
-              <div className="col-lg-5">
-                <div className="section-disc">
-                  Aperiam sociosqu urna praesent, tristique, corrupti
-                  condimentum asperiores platea ipsum ad arcu. Nostrud. Aut
-                  nostrum, ornare quas provident laoreet nesciunt.
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="destination-inner destination-three-column">
-            <div className="row">
-              <div className="col-lg-7">
-                <div className="row">
-                  <div className="col-sm-6">
-                    <div className="desti-item overlay-desti-item">
-                      <figure className="desti-image">
-                        <img
-                          className="w-full"
-                          src="../../../../assets/images/img1.jpg"
-                          alt=""
-                        />
-                      </figure>
-                      <div className="meta-cat bg-meta-cat">
-                        <a href="#">THAILAND</a>
-                      </div>
-                      <div className="desti-content">
-                        <h3>
-                          <a href="#">Disney Land</a>
-                        </h3>
-                        <div className="rating-start" title="Rated 5 out of 4">
-                          <span className="w-1/2"></span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-sm-6">
-                    <div className="desti-item overlay-desti-item">
-                      <figure className="desti-image">
-                        <img
-                          className="w-full"
-                          src="../../../../assets/images/img2.jpg"
-                          alt=""
-                        />
-                      </figure>
-                      <div className="meta-cat bg-meta-cat">
-                        <a href="#">NORWAY</a>
-                      </div>
-                      <div className="desti-content">
-                        <h3>
-                          <a href="#">Besseggen Ridge</a>
-                        </h3>
-                        <div className="rating-start" title="Rated 5 out of 5">
-                          <span className="w-full"></span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-5">
-                <div className="row">
-                  <div className="col-md-6 col-xl-12">
-                    <div className="desti-item overlay-desti-item">
-                      <figure className="desti-image">
-                        <img
-                          className="w-full"
-                          src="../../../../assets/images/img3.jpg"
-                          alt=""
-                        />
-                      </figure>
-                      <div className="meta-cat bg-meta-cat">
-                        <a href="#">NEW ZEALAND</a>
-                      </div>
-                      <div className="desti-content">
-                        <h3>
-                          <a href="#">Oxolotan City</a>
-                        </h3>
-                        <div className="rating-start" title="Rated 5 out of 5">
-                          <span className="w-full"></span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-6 col-xl-12">
-                    <div className="desti-item overlay-desti-item">
-                      <figure className="desti-image">
-                        <img
-                          className="w-full"
-                          src="../../../../assets/images/img4.jpg"
-                          alt=""
-                        />
-                      </figure>
-                      <div className="meta-cat bg-meta-cat">
-                        <a href="#">SINGAPORE</a>
-                      </div>
-                      <div className="desti-content">
-                        <h3>
-                          <a href="#">Marina Bay Sand City</a>
-                        </h3>
-                        <div
-                          className="rating-start"
-                          title="Rated 5 out of 4"
-                        ></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="btn-wrap text-center">
-              <a href="#" className="button-primary">
-                MORE DESTINATION
-              </a>
-            </div>
-          </div>
-        </div>
-      </section> */}
+
       {/* <!-- Home packages section html start --> */}
       <section className="package-section">
         <div className="container">
