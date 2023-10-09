7c9e1ce160a4fa2fe8f4b6c61ef1098a3aeb89ee
thaivhph28695 <hongthai96961996@gmail.com>
2023-10-10 00:14:44 +0700

    update tour price when change value

--- FE-VCDTT/src/api/blogs.ts
+++ FE-VCDTT/src/api/blogs.ts
@@ -8,7 +8,7 @@ const BlogApi = createApi({
     reducerPath: "blog",
     tagTypes: ['Blog'],
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://be-vcdtt.datn-vcdtt.test/api/admin/",
+        baseUrl: "http://be-vcdtt.datn-vcdtt.test/api/",
         fetchFn: async (...arg) => {
             await pause(1000)
             return await fetch(...arg);
--- FE-VCDTT/src/api/tours.ts
+++ FE-VCDTT/src/api/tours.ts
@@ -8,7 +8,7 @@ const TourApi = createApi({
     reducerPath: "tour",
     tagTypes: ['Tour'],
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://be-vcdtt.datn-vcdtt.test/api/admin/",
+        baseUrl: "http://be-vcdtt.datn-vcdtt.test/api/",
         fetchFn: async (...arg) => {
             await pause(1000)
             return await fetch(...arg);
