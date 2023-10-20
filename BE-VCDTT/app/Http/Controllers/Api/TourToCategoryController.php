166633a1f437274e0192ef60f9e598af69a4e85a
chinhph28756 <chinhddph28756@fpt.edu.vn>
2023-10-20 23:30:05 +0700

    fetaure show tours by cate_id

--- BE-VCDTT/app/Http/Controllers/Api/TourToCategoryController.php
+++ BE-VCDTT/app/Http/Controllers/Api/TourToCategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TourResource;
+use App\Models\Tour;
 use Illuminate\Http\Request;
 
 class TourToCategoryController extends Controller
@@ -28,7 +30,25 @@ class TourToCategoryController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $query = Tour::select('tours.id', 'tours.name', 'tours.duration', 'tours.child_price', 'tours.adult_price', 'tours.sale_percentage', 'tours.start_destination', 'tours.end_destination', 'tours.tourist_count', 'tours.details', 'tours.location', 'tours.exact_location', 'tours.pathway', 'tours.main_img', 'tours.view_count', 'tours.status', 'tours.created_at', 'tours.updated_at')
+                ->join('tours_to_categories', 'tours.id', '=', 'tours_to_categories.tour_id')
+                ->where('tours_to_categories.cate_id', $id)
+                ->groupBy('tours.id')
+                ->orderBy('tours.id', 'ASC');
+
+        $toursByCate = $query->get();
+
+        return response()->json(
+            [
+                'data' => [
+                    'toursByCate' => new TourResource($toursByCate)
+                ],
+                'message' => 'OK',
+                'status' => 200
+
+            ]
+        );
+        
     }
 
     /**
