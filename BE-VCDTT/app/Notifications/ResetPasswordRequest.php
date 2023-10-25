9d065cf81382fd79d97cf13fcc1c0a29679d81c0
Nguyễn Tiến Đạt <secretangent2112@gmail.com>
2023-10-25 10:31:25 +0700

    sửa text mail

--- BE-VCDTT/app/Notifications/ResetPasswordRequest.php
+++ BE-VCDTT/app/Notifications/ResetPasswordRequest.php
@@ -43,8 +43,8 @@ class ResetPasswordRequest extends Notification implements ShouldQueue
         $url = 'http://datn-vcdtt.test:5173/reset-password/' . $this->token;
 
         return (new MailMessage)
-            ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', $url)
-            ->line('If you did not request a password reset, no further action is required.');
+            ->line('Bạn nhận được mail này vì có một yêu cầu đổi mật khẩu cho tài khoản của bạn')
+            ->action('Đổi mật khẩu', $url)
+            ->line('Nếu không phải là bạn, vui lòng bỏ qua email này');
     }
 }
