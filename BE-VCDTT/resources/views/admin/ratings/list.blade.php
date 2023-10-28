35eae5928f18276f83e88ba81d9cfd35f60d56f6
congvu-xconnecting <92044924+VuChiDoanh@users.noreply.github.com>
2023-10-28 13:36:47 +0700

    ratting of a tour action

--- BE-VCDTT/resources/views/admin/ratings/list.blade.php
+++ BE-VCDTT/resources/views/admin/ratings/list.blade.php
@@ -118,15 +118,26 @@
                                                     {{ time_format($data->updated_at) }}
                                                 </td>
                                                 <td class="text-end">
-                                                    <span class="dropdown">
-                                                        <button class="btn dropdown-toggle align-text-top"
-                                                            data-bs-boundary="viewport"
-                                                            data-bs-toggle="dropdown">Hành động</button>
-                                                        <div class="dropdown-menu dropdown-menu-end">
-                                                            <a class="dropdown-item" href="{{ route('rating.edit', ['id' => $data->id]) }}">Trả lời đánh giá</a>
-                                                            <a class="dropdown-item" href="javascript: removeItem({{ $data->id}})">Xóa</a>
-                                                        </div>
-                                                    </span>
+                                                    <a class="btn btn-icon btn-outline-green" href="{{ route('rating.edit', ['id' => $data->id]) }}">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-message-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                        <path d="M8 9h8"></path>
+                                                        <path d="M8 13h6"></path>
+                                                        <path d="M12.01 18.594l-4.01 2.406v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v5.5"></path>
+                                                        <path d="M16 19h6"></path>
+                                                        <path d="M19 16v6"></path>
+                                                        </svg>
+                                                    </a>
+                                                    <a class="btn btn-icon btn-outline-red" href="javascript: removeItem({{ $data->id}})">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                        <path d="M4 7l16 0"></path>
+                                                        <path d="M10 11l0 6"></path>
+                                                        <path d="M14 11l0 6"></path>
+                                                        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+                                                        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+                                                        </svg>
+                                                    </a>
                                                 </td>
                                             </tr>
                                         @endforeach
