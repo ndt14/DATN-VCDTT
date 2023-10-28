310bb0e2aa8902e4f097c2322e6300f927ed5d75
congvu-xconnecting <92044924+VuChiDoanh@users.noreply.github.com>
2023-10-28 13:08:38 +0700

    purchase list

--- BE-VCDTT/resources/views/admin/purchase_histories/list.blade.php
+++ BE-VCDTT/resources/views/admin/purchase_histories/list.blade.php
@@ -169,17 +169,24 @@
                                                     @endswitch
                                                 </td>
                                                 <td class="text-end">
-                                                    <span class="dropdown">
-                                                        <button class="btn dropdown-toggle align-text-top"
-                                                            data-bs-boundary="viewport"
-                                                            data-bs-toggle="dropdown">Hành động</button>
-                                                        <div class="dropdown-menu dropdown-menu-end">
-                                                            <a class="dropdown-item"
-                                                                href=" {{ route('purchase_histories.edit', ['id' => $item['id']]) }}">Chỉnh sửa</a>
-                                                            {{-- <a class="dropdown-item"
-                                                                href="javascript: removeItem({{ $item['id'] }})">Remove</a> --}}
-                                                        </div>
-                                                    </span>
+                                                    <a class="btn btn-icon btn-outline-green" href=" {{ route('purchase_histories.edit', ['id' => $item['id']]) }}">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                        <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1"></path>
+                                                        <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z"></path>
+                                                        <path d="M16 5l3 3"></path>
+                                                        </svg>
+                                                    </a>
+                                                    <a class="btn btn-icon btn-outline-red" href="javascript: removeItem({{ $item['id'] }})">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                        <path d="M4 7l16 0"></path>
+                                                        <path d="M10 11l0 6"></path>
+                                                        <path d="M14 11l0 6"></path>
+                                                        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+                                                        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+                                                        </svg>
+                                                    </a>
                                                 </td>
                                             </tr>
                                         @endforeach
