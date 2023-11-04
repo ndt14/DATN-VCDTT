d8611033c90a52daaa11d97185155481a6e1eb89
congvu-xconnecting <92044924+VuChiDoanh@users.noreply.github.com>
2023-11-04 15:00:51 +0700

    image manage

---
+++ BE-VCDTT/resources/views/admin/Images/add.blade.php
@@ -0,0 +1,100 @@
+@extends('admin.common.layout')
+@section('content')
+
+<div class="page-header d-print-none">
+    <div class="container-xl">
+        <div class="row g-2 align-items-center">
+            <div class="col-auto">
+                <!-- Page pre-title -->
+                <!-- <div class="page-pretitle">
+                    Overview
+                </div> -->
+                <h1 class="text-primary mb-4" style="font-size: 36px;">
+                    Quản lý ảnh
+                </h2>
+            </div>
+            <div class="top-0" style="z-index: 100; position: fixed;">
+                @if (Session::has('success'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert" id="notiSuccess">
+                    {{ Session::get('success') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                @endif
+                @if (Session::has('fail'))
+                <div class="alert alert-danger alert-dismissible fade show" role="alert" id="notiError">
+                    {{ Session::get('fail') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                @endif
+            </div>
+            <!-- Page title actions -->
+            <div class="col-auto ms-auto d-print-none">
+                <div class="btn-list">
+
+
+                        <a href="{{route('image.list')}}" class="btn btn-default d-none d-sm-inline-block">
+
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-narrow-left" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M5 12l14 0"></path>
+                                <path d="M5 12l4 4"></path>
+                                <path d="M5 12l4 -4"></path>
+                            </svg>
+                            Quay lại
+                        </a>
+                        <a href="{{url('/image')}}" class="btn btn-default d-sm-none btn-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-narrow-left" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M5 12l14 0"></path>
+                                <path d="M5 12l4 4"></path>
+                                <path d="M5 12l4 -4"></path>
+                            </svg>
+                        </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="page-body">
+    <div class="container-xl">
+        <div class="row row-deck row-cards">
+            <div class="col-sm-12 col-md-8 offset-md-2">
+                <form id="frmAdd" class="card border-0 shadow-lg rounded-4 " action="{{route('image.store')}}" method="POST">
+                    @csrf
+                    <div class="card-body">
+                        <div class="mb-3">
+                            <label class="form-label">Tên danh mục</label>
+                            <input type="text" name="name" class="form-control" placeholder="Nhập tên danh mục" value="">
+                            <span class="text-danger d-flex justify-content-start">
+                                @error('name')
+                                {{ $message }}
+                                @enderror
+                            </span>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Thuộc danh mục cha</label>
+
+                            <select class="form-select" name="parent_id" {{ count($data) <= 0 ? 'disabled' : '' }}>
+                                <option value="">Lựa chọn</option>
+                                @foreach($data as $item)
+                                <option value="{{$item->id}}">{{$item->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                    </div>
+                    <div class="card-footer text-right">
+                        <button id="btnSubmitAdd" type="submit" class="btn btn-primary">Thêm mới</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@section('page_js')
+<script type="text/javascript">
+
+</script>
+@endSection
--- BE-VCDTT/resources/views/admin/common/sidebar.blade.php
+++ BE-VCDTT/resources/views/admin/common/sidebar.blade.php
@@ -198,6 +198,22 @@
             </div>
         </li>
         @endif
+        <li class="nav-item py-lg-2">
+          <a class="nav-link" href="{{route('image.list')}}">
+            <span class="me-1 d-md-none d-lg-inline-block">
+              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-photo" style="margin-bottom: 2px;" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M15 8h.01"></path>
+                <path d="M3 6a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v12a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3v-12z"></path>
+                <path d="M3 16l5 -5c.928 -.893 2.072 -.893 3 0l5 5"></path>
+                <path d="M14 14l1 -1c.928 -.893 2.072 -.893 3 0l3 3"></path>
+              </svg>
+            </span>
+            <span class="fw-bold ms-1 fs-3">
+              Thư viện ảnh
+            </span>
+          </a>
+        </li>
       </ul>
     </div>
   </div>
