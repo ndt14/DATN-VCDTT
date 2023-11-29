72380fbe459b4e7a1941ca8df20500edb506ec18
ndt14 <thinh1420003@gmail.com>
2023-11-29 09:08:25 +0700

    contact us

--- FE-VCDTT/src/api/setting.ts
+++ FE-VCDTT/src/api/setting.ts
@@ -16,21 +16,57 @@ const SettingApi = createApi({
         }
     }),
     endpoints: (builder) => ({
-        getLogo: builder.query<Setting[], void>({
+        getLogo: builder.query<Setting, void>({
             query: () => `/keyvalue/logo`,
             providesTags: ['Keyvalue']
         }),
-        getBankName: builder.query<Setting[], void>({
+        getBankName: builder.query<Setting, void>({
             query: () => `/keyvalue/bankName`,
             providesTags: ['Keyvalue']
         }),
-      
+        getBanner: builder.query<Setting, void>({
+            query: () => `/keyvalue/banner`,
+            providesTags: ['Keyvalue']
+        }),
+        getBankAccountName: builder.query<Setting, void>({
+            query: () => `/keyvalue/bankAccountName`,
+            providesTags: ['Keyvalue']
+        }),
+        getEmailWeb: builder.query<Setting, void>({
+            query: () => `/keyvalue/email`,
+            providesTags: ['Keyvalue']
+        }),
+        getFavicon: builder.query<Setting, void>({
+            query: () => `/keyvalue/favicon`,
+            providesTags: ['Keyvalue']
+        }),
+        getAddress: builder.query<Setting, void>({
+            query: () => `/keyvalue/address`,
+            providesTags: ['Keyvalue']
+        }),
+        getWebPhoneNumber1: builder.query<Setting, void>({
+            query: () => `/keyvalue/webPhoneNumber1`,
+            providesTags: ['Keyvalue']
+        }),
+        getWebTitle: builder.query<Setting, void>({
+            query: () => `/keyvalue/webTitle`,
+            providesTags: ['Keyvalue']
+        }),
     })
 });
 
 export const {
     useGetLogoQuery,
-    useGetBankNameQuery
+    useGetBankNameQuery,
+    useLazyGetBannerQuery,
+    useGetBankAccountNameQuery,
+    useGetAddressQuery,
+    useGetEmailWebQuery,
+    useGetBannerQuery,
+    useGetFaviconQuery,
+    useGetWebPhoneNumber1Query,
+    useGetWebTitleQuery,
+
  } = SettingApi;
 export const settingReducer = SettingApi.reducer;
 export default SettingApi;
