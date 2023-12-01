101cff17f3dfebcdacb95a3f479cb1d9f48ff42c
congvu-xconnecting <92044924+VuChiDoanh@users.noreply.github.com>
2023-12-02 01:22:43 +0700

    fix done

---
+++ BE-VCDTT/resources/views/admin/images/list.blade.php
@@ -0,0 +1,296 @@
+@extends('admin.common.layout')
+@section('meta_title')
+Thư viện ảnh
+@endSection
+@section('content')
+    <div class="page-header d-print-none">
+        <div class="container-xl">
+            <div class="row g-2 align-items-center">
+                <div class="col">
+                    <h1 class="text-indigo mb-4" style="font-size: 36px;">
+                        Quản lý thư viện ảnh
+                    </h1>
+                </div>
+                <!-- <div class="col-12 ">
+                    @if (Session::has('success'))
+                        <div class="alert alert-success alert-dismissible fade show" role="alert" id="notiSuccess">
+                            {{ Session::get('success') }}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    @endif
+                    @if (Session::has('fail'))
+                        <div class="alert alert-success alert-dismissible fade show" role="alert" id="notiError">
+                            {{ Session::get('fail') }}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    @endif
+                </div> -->
+                <div class="col-auto ms-auto d-print-none">
+                    <div class="btn-list">
+                        <a href="/image/dropzone" class="btn btn-indigo d-none d-sm-inline-block">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <line x1="12" y1="5" x2="12" y2="19" />
+                                <line x1="5" y1="12" x2="19" y2="12" />
+                            </svg>
+                           Thêm mới
+                        </a>
+                        <a href="/image/dropzone}" class="btn btn-indigo d-sm-none btn-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <line x1="12" y1="5" x2="12" y2="19" />
+                                <line x1="5" y1="12" x2="19" y2="12" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Page body -->
+    <div class="page-body">
+        <div class="container-xl">
+            <div class="row row-deck row-cards">
+                <div class="col-12">
+                    <div class="card border-0 shadow-lg rounded-4 ">
+                        <div class="card-header">
+                            <h3 class="card-title">Ảnh</h3> <a href="" style="padding-left: 5px; text-decoration: none;"><span style="color: black;">|</span> Thùng rác</a>
+                        </div>
+                        <div class="card-body border-bottom py-3">
+                            <div class="d-flex">
+                                <!--<div class="text-muted">
+                                    Show
+                                    <div class="mx-2 d-inline-block">
+                                        <input type="text" class="form-control form-control-sm" value="8" size="3" aria-label="Invoices count">
+                                    </div>
+                                    entries
+                                </div>-->
+                                <div class="ms-auto text-muted">
+                                    <form method="get" action="" class="row gy-2 gx-3 align-items-center">
+                                        <div class="col-auto">
+                                            <label class="visually-hidden" for="autoSizingSelect">Trạng thái</label>
+                                        <select class="form-select" name="lang_code">
+                                            <option value="">Chọn trạng thái</option>
+                                            <option value="ja">Đang hoạt động</option>
+                                            <option value="en">Không hoạt động</option>
+                                        </select>
+                                        </div>
+                                        <div class="col-auto">
+                                            <label class="visually-hidden" for="autoSizingInput">Từ khóa</label>
+                                            <input type="text" name="keyword" value="" class="form-control" placeholder="Keyword">
+                                        </div>
+                                        <div class="col-auto">
+                                            <button type="submit" class="btn btn-indigo">Tìm</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="table table-responsive">
+                            <table class="table card-table table-vcenter text-nowrap datatable">
+                                <thead>
+                                    <tr>
+                                        <th class="">Ảnh</th>
+                                        <th class="w-1">Áp dụng cho</th>
+                                        <th>Tên Ảnh</th>
+                                        <th>Định dạng</th>
+                                        <th>Đường dẫn</th>
+                                        <th>Ngày thêm</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @if ($data)
+                                        @foreach ($data as $item)
+                                            <tr>
+                                                <td>
+                                                <a data-fancybox data-src="{{ $item->url}}" href="javascript:void(0);">
+                                                    <img style="width: 150px; height: 90px; object-fit: cover;" src="{{ $item->url}}" alt="{{ $item->name}}">
+                                                </a>
+                                            </td>
+                                                <td><span class="text-muted">{{ string_truncate($item->tour_name, 70) }}</span></td>
+                                                <td>
+                                                {{ string_truncate($item->name, 70) }}
+                                                </td>
+                                                                                            <td>
+                                                {{ $item->type }}
+                                            </td>
+                                            <td>
+                                                {{ $item->url }}
+                                            </td>
+                                            <td>
+                                                {{ time_format($item->created_at) }}
+                                                </td>
+                                                <td class="text-end">
+                                                    <a href="javascript: void(0);" data-url="{{ url($item->url)}}" class="btn btn-icon btn-outline-indigo btn-copy-url" title="Sao chép đường dẫn nội tuyến">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-link" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                        <path d="M9 15l6 -6"></path>
+                                                        <path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464"></path>
+                                                        <path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463"></path>
+                                                        </svg>
+                                                    </a>
+                                                    <a class="btn btn-icon btn-outline-indigo" href="/image-download/{{ $item->id}}" title="Tải xuống">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-download" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                        <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2"></path>
+                                                        <path d="M7 11l5 5l5 -5"></path>
+                                                        <path d="M12 4l0 12"></path>
+                                                        </svg>
+                                                    </a>
+                                                    <a class="btn btn-icon btn-outline-red" href="javascript: removeItem({{ $item->id}})" title="Xoá">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                        <path d="M4 7l16 0"></path>
+                                                        <path d="M10 11l0 6"></path>
+                                                        <path d="M14 11l0 6"></path>
+                                                        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+                                                        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+                                                        </svg>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                                                                    @endforeach
+                                    @else
+                                        <tr>
+                                            <td colspan="9">
+                                                <div>Không có dữ liệu</div>
+                                            </td>
+                                        </tr>
+                                    @endif
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="card-footer d-flex align-items-center">
+@php
+                                $pageLimits = [5,10,20,50,100,250,300];
+                            @endphp
+                            <select id="rpp" class="form-select me-2" style="max-width: 75px;">
+                                @foreach ($pageLimits as $p)
+                                <option {{ $data->perPage() == $p?'selected':'' }} value="{{ $p }}">{{ $p }}</option>
+                                @endforeach
+                            </select>
+
+                            <p class="m-0 text-secondary">Hiển thị <span>{{ $data->currentPage() }}</span> trên <span>{{ $data->lastPage() }}</span> của <span>{{ $data->total() }}</span>
+                                bản ghi</p>
+
+                            <ul class="pagination m-0 ms-auto">
+                                <li class="page-item {{ $data->currentPage() != 1 ? '' : 'disabled' }}">
+                                    <a class="page-link" href="{{ $data->previousPageUrl()}}" tabindex="-1" aria-disabled="true">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
+                                            height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <path d="M15 6l-6 6l6 6"></path>
+                                        </svg>prev</a>
+                                </li>
+                                <li class="page-item {{ $data->currentPage() == 1 ? 'active' : '' }}">
+                                    <a class="page-link" href="{{ $data->url(1) }}">1</a>
+                                </li>
+                                @for ($page = max(2, $data->currentPage()-2); $page <= $data->currentPage()+2 && $page <= $data->lastPage()-1; $page++)
+
+                                    <li class="page-item {{ $page == $data->currentPage() ? 'active' : '' }}">
+                                        <a class="page-link" href="{{ $data->url($page) }}">{{ $page }}</a>
+                                    </li>
+
+                                @endfor
+                                @if($data->currentPage()+3 != $data->lastPage() && $data->lastPage() >3)
+                                <li class="page-item">
+                                        ...
+                                </li>
+                                @endif
+                                <li class="page-item {{ $page == $data->currentPage() ? 'active' : '' }}">
+                                    <a class="page-link" href="{{ $data->url($data->lastPage()) }}">{{ $data->lastPage() }}</a>
+                                </li>
+                                <li class="page-item {{ $data->currentPage() != $data->lastPage() ? '' : 'disabled' }}">
+                                    <a class="page-link" href="{{ $data->nextPageUrl()}}">Next
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
+                                            height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <path d="M9 6l6 6l-6 6"></path>
+                                        </svg>
+                                    </a>
+                                </li>
+                            </ul>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal modal-blur fade" id="modalContainer" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+            </div>
+        </div>
+    </div>
+@endSection
+@section('page_css')
+<link rel="stylesheet" href="{{ asset('admin/assets/css/fancybox.css') }}"/>
+@endSection
+@section('page_js')
+<script src="{{ asset('admin/assets/js/vendors/clipboard-polyfill.window-var.promise.es5.js') }}"></script>
+<script src="{{ asset('admin/assets/js/vendors/fancybox.umd.js') }}"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            modalContainer = new bootstrap.Modal('#modalContainer', {
+                keyboard: true,
+                backdrop: 'static'
+            });
+
+            Fancybox.bind('[data-fancybox]');
+            $('.btn-copy-url').click(function () {
+                let _self = $(this);
+                let url = _self.attr('data-url');
+                clipboard.writeText(url).then(function(){
+                    bs5Utils.Snack.show('success', 'Đã copy đường dẫn thành công!', delay = 5000, dismissible = true);
+                }, function(err){
+                    bs5Utils.Snack.show('danger', 'Lỗi.', delay = 5000, dismissible = true);
+                });
+            });
+        });
+
+        let viewDetail = function(id) {
+        axios.get(`/faq/detail/${id}`)
+            .then(function(response) {
+                $('#modalContainer div.modal-content').html(response.data.html);
+                modalContainer.show();
+            })
+            .catch(function(error) {
+                bs5Utils.Snack.show('danger', 'Error', delay = 5000, dismissible = true);
+            })
+            .finally(function() {
+            });
+        };
+
+        let removeItem = function(id) {
+        $.confirm({
+            theme: theme,
+            title: 'Confirm',
+            content: 'Are you sure to remove?',
+            columnClass: 'col-md-3 col-sm-6',
+            buttons: {
+                removeButton: {
+                    text: 'Yes',
+                    btnClass: 'btn-danger',
+                    action: function() {
+                        axios.delete(`/image-destroy/${id}`).then(function(response) {
+                            bs5Utils.Snack.show('success', 'Success', delay = 5000, dismissible = true);
+                            setTimeout(() => {
+                                location.reload();
+                            }, 2000);
+                        });
+                    }
+                },
+                close: function() {}
+            }
+        });
+    };
+    </script>
+@endSection
